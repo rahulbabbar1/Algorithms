ae4750a91ee0abca764bfc998d04bb4e22505c7d
Rahul Babbar <rahulbabbar1@live.com>
2017-02-16 23:45:22 +0530

    Added searchInterval Method

--- interval_tree.cpp
+++ interval_tree.cpp
@@ -5,11 +5,11 @@ struct node{
 	int low, high, max_high;
 	node *left;
 	node *right;
-	node(int l, int r)
+	node()
 	{
-		low = l;
-		high = r;
-		max_high = r;
+		low = 0;
+		high = 0;
+		max_high = 0;
 		left = NULL;
 		right = NULL;
 	}
@@ -19,7 +19,12 @@ node* insertInterval(node *root,int l,int r)
 {
     if(root==NULL)
     {
-    	node *t = new node(l,r);
+    	// node *t = new node(l,r);
+        // root = t;
+        node *t = new node;
+        t->low = l;
+        t->high = r;
+        t->max_high = r;
         root = t;
     }
     else if(l > root->low )
@@ -44,17 +49,29 @@ void printIntervals(node *intervals)
     printIntervals(intervals->right);
 }
 
+node* searchInterval(node *intervals,int l,int r)
+{
+	if (intervals == NULL)
+        return NULL;
+    if (intervals->low<=r && intervals->high>=l)
+        return intervals;
+    if (intervals->left != NULL && (intervals->left)->max_high >= l)
+        return searchInterval(intervals->left, l, r);
+    return searchInterval(intervals->right, l, r);
+
+}
 
 int main(){
 	int n;
 	cin>>n;
-	node *intervals;
+	node *intervals = new node;
+	intervals=NULL;
 	for(int i=0;i<n;i++){
 		int low,high;
 		cin>>low>>high;
 		intervals = insertInterval(intervals, low, high);
 	}
-	
+	// printIntervals(intervals);
 	while(true){
 		cout<<"Press 1 to print, 2 to insert, 3 to search, 4 to delete, 5 to exit "<<"\n";
 		int type;
@@ -66,6 +83,15 @@ int main(){
 			int low,high;
 			cin>>low>>high;
 			insertInterval(intervals,low,high);
+		}
+		else if(type==3){
+			int low,high;
+			cin>>low>>high;
+			node *interval = searchInterval(intervals,low,high);
+			if(interval==NULL)
+				cout<<"Interval not found"<<"\n";
+			else
+				cout<<"Interval found with low = "<<interval->low<<", high = "<<interval->high<<" and max = "<<interval->max_high<<"\n";
 		}
 		else
 			break;
