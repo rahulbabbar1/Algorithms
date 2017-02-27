d87ac8fb7b1b52f658d7c4fbdceef7a661bc15d1
Rahul Babbar <rahulbabbar1@live.com>
2017-02-27 23:00:06 +0530

    Completed ques 2 and updated readme

--- Assignment 2/README.md
+++ Assignment 2/README.md
@@ -1,8 +1,56 @@
 # Assignment - 2
 ## Ques 1
 [Problem Statement for ZigZag on TopCoder Community](https://community.topcoder.com/stat?c=problem_statement&pm=1259)
+
 [Question 1 Solution at Ideone.com](http://ideone.com/x9Q65x)
+
+###Input:
+	4
+	6   
+	1 7 4 9 2 5
+	10
+	1 17 5 10 13 15 10 5 16 8
+	1
+	44
+	9 
+	1 2 3 4 5 6 7 8 9
+
+###Output:
+	6
+	7
+	1
+	2
+
+
+## Ques 2
+[Problem Statement for AvoidRoads on TopCoder Community](https://community.topcoder.com/stat?c=problem_statement&pm=1889)
+
+[Question 2 Solution at Ideone.com](http://ideone.com/mdfsSk)
+
+###Input:
+	6
+	6
+	{"0 0 0 1","6 6 5 6"}
+
+###Output:
+	252
 
 ## Ques 3
 [Problem Statement for The Lucky Draw on Codechef](https://www.codechef.com/problems/D2/)
+
 [Question 3 Solution at Ideone.com](http://ideone.com/PuPK0R)
+
+###Input:
+	3
+	2
+	0 0
+	3
+	3 2 1
+	6
+	4 8 6 1 5 2
+
+###Output:
+	1
+	2
+	4
+
--- Assignment 2/ques_2.cpp
+++ Assignment 2/ques_2.cpp
@@ -15,7 +15,19 @@ vector<string> split(const string &s, char delim = ',')
     }
     return elems;
 }
+int strtoint(string s)
+{	
+	int num = 0;
+	for(int i=0;i<s.size();i++)
+	{
+		num = num*10;
+		num += (int)s[i]-48;
+	}
+	return num;
+}
 
+long long int dp[101][101] = {0};
+bool isBlocked[101][101] = {0};
 int main() {
 	// your code goes here
 	int a,b;
@@ -25,13 +37,46 @@ int main() {
 	getline(cin,s,'}');
 	
 	s = s.substr(2);
-	//remove '{' from string
 
 	vector<string> vect = split(s,',');
 
-	cout<<vect[0];
+	for(int i=0;i<vect.size();i++){
+		string pString = vect[i];
+		pString = pString.substr(1,pString.length()-2);
+		vector<string> point = split(pString,' ');
+		//cout<<point[0]<<" "<<point[1]<<" "<<point[2]<<" "<<point[3]<<"\n";
+		int p1 = strtoint(point[0]);
+		int p2 = strtoint(point[1]);
+		int p3 = strtoint(point[2]);
+		int p4 = strtoint(point[3]);
+		isBlocked[p1][p2] = 1;
+		isBlocked[p3][p4] = 1;
+	}
+	dp[0][0] = 1;
+	for(int i=0;i<=a;i++)
+	{
+		for(int j=0;j<=b;j++)
+		{
+			if(isBlocked[i][j])
+			{
+				if(!isBlocked[i+1][j])
+				{
+					dp[i+1][j] += dp[i][j];
+				}
+				if(!isBlocked[i][j+1])
+				{
+					dp[i][j+1] += dp[i][j];
+				}
+			}
+			else
+			{
+				dp[i+1][j] += dp[i][j];
+				dp[i][j+1] += dp[i][j];
+			}
+		}
+	}
+	cout << dp[a][b] << endl;
 
-	
 
 	// string delimiter = "";
 
@@ -44,6 +89,5 @@ int main() {
 	// }
 	// cout << s << endl;
 
-
-
+	return 0;
 }
